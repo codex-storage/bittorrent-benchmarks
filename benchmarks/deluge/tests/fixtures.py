1261dd08de47493d203a24fa44e4b8335727d94f
gmega <giuliano.mega@gmail.com>
2024-12-03 19:33:43 -0300

    make it possible to run the test runner containerized; add integration tests to CI

--- benchmarks/deluge/tests/fixtures.py
+++ benchmarks/deluge/tests/fixtures.py
@@ -1,8 +1,9 @@
+import os
 from pathlib import Path
 from typing import Generator
 
 import pytest
-from urllib3.util import Url, parse_url
+from urllib3.util import parse_url
 
 from benchmarks.core import utils
 from benchmarks.core.utils import megabytes, await_predicate
@@ -11,9 +12,9 @@ from benchmarks.deluge.tracker import Tracker
 from benchmarks.tests.utils import shared_volume
 
 
-def deluge_node(name: str, port: int) -> Generator[DelugeNode, None, None]:
-    node = DelugeNode(name, volume=shared_volume(), daemon_port=port)
-    await_predicate(node.is_ready, timeout=10, polling_interval=0.5)
+def deluge_node(name: str, address: str, port: int) -> Generator[DelugeNode, None, None]:
+    node = DelugeNode(name, volume=shared_volume(), daemon_address=address, daemon_port=port)
+    assert await_predicate(node.is_ready, timeout=10, polling_interval=0.5)
     node.wipe_all_torrents()
     try:
         yield node
@@ -23,17 +24,17 @@ def deluge_node(name: str, port: int) -> Generator[DelugeNode, None, None]:
 
 @pytest.fixture
 def deluge_node1() -> Generator[DelugeNode, None, None]:
-    yield from deluge_node('deluge-1', 6890)
+    yield from deluge_node('deluge-1', os.environ.get('DELUGE_NODE_1', 'localhost'), 6890)
 
 
 @pytest.fixture
 def deluge_node2() -> Generator[DelugeNode, None, None]:
-    yield from deluge_node('deluge-2', 6893)
+    yield from deluge_node('deluge-2', os.environ.get('DELUGE_NODE_2', 'localhost'), 6893)
 
 
 @pytest.fixture
 def deluge_node3() -> Generator[DelugeNode, None, None]:
-    yield from deluge_node('deluge-3', 6896)
+    yield from deluge_node('deluge-3', os.environ.get('DELUGE_NODE_3', 'localhost'), 6896)
 
 
 @pytest.fixture
@@ -44,4 +45,4 @@ def temp_random_file() -> Generator[Path, None, None]:
 
 @pytest.fixture
 def tracker() -> Tracker:
-    return Tracker(parse_url('http://127.0.0.1:8000/announce'))
+    return Tracker(parse_url(os.environ.get('TRACKER_ANNOUNCE_URL', 'http://127.0.0.1:8000/announce')))
